1a7e46b8ac26a7ef63cdb5a5d64fef8bc8bc631f
Levi Walton <22774739+leftengeist@users.noreply.github.com>
2020-02-03 15:37:57 -0500

    Added Files

---
+++ Lab1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Grading ID: N5284
+//CIS 199-75
+//Lab 1
+//Due: January 7th, 2019
+//This program prints my Grading ID, Hobbies, Favorite Book, and Favorite Movie
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grading ID: N5284");
+            Console.WriteLine("Hobbies: Programming and Travel");
+            Console.WriteLine("Favorite Book: George Orwell's 1984");
+            Console.WriteLine("Favorite Movie: The Immitation Game");
+        }// End of Main
+    }// End of Program
+}//End of Lab1
